db32f555a775b544486323aa1cae6efcec6cb5c2
jackie chan ;) <necko.lm@gmail.com>
2021-02-02 14:47:05 +0100

    jackie chan ;)

---
+++ vue_demineur.c
@@ -0,0 +1,27 @@
+/******************************************************************************
+ * vue_demineur.c
+ *
+ * Création de la vue du jeu Demineur.
+ *
+ * cree le     : 19/01/2020
+ * par         : Nicolas LE MOINE <necko.lm@gmail.com>
+ *
+ * Compilation : gcc vue_demineur.c -Wall `pkg-config gtk+-3.0 --cflags --libs`
+ *
+ * modifie le  :
+ * par         :
+ * description :  
+ ******************************************************************************/
+
+#include <stdio.h>
+#include <math.h>
+#include <gtk/gtk.h>
+#include "vue_demineur.h"
+
+/*---------------------------------------------------- constructeur */
+
+
+
+/*----------------------------------------------------- destructeur */
+
+
